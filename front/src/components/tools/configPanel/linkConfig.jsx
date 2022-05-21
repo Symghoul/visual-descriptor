ba2b306bd29ea38d47633c04e46f61eb6c4c7386
ZornStolz <1143875103@u.icesi.edu.co>
2022-05-21 18:21:42 -0500

    Devices are added in canvas and db, its attributes can be modified and changes persist in db, devices can be deleted from canvas and db

--- front/src/components/tools/configPanel/linkConfig.jsx
+++ front/src/components/tools/configPanel/linkConfig.jsx
@@ -1,17 +1,16 @@
 import React, { useContext, useEffect, useState } from "react";
 import { ThemeProvider } from "@mui/material/styles";
 import { theme, CssTextField } from "../../../config/theme";
-import { Button, TextField } from "@mui/material";
-
-import "./linkConfig.css";
+import { Button } from "@mui/material";
 import AppContext from "../../../context/AppContext";
+import "./linkConfig.css";
 
 const LinkConfig = () => {
   const state = useContext(AppContext);
 
   const [delay, setDelay] = useState("");
   const [loss, setLoss] = useState("");
-  const [bandwith, setBandwith] = useState("");
+  const [bandwidth, setBandwidth] = useState("");
 
   /**
    * Fill the editable fields
@@ -22,49 +21,105 @@ const LinkConfig = () => {
 
       setDelay(device.delay);
       setLoss(device.loss);
-      setBandwith(device.setBandwith);
+      setBandwidth(device.bandwidth);
     }
   }, [state.selectedDevice]);
 
+  /**
+   * handle delay change
+   */
+  useEffect(() => {
+    let oldLink = state.getDevice(state.selectedDevice);
+    let update = { ...oldLink, delay };
+
+    const arr = state.links.map((link) => {
+      if (link.id === oldLink.id) {
+        return update;
+      }
+      return link;
+    });
+
+    state.setLinks(arr);
+  }, [delay]);
+
+  /**
+   * handle loss change
+   */
+  useEffect(() => {
+    let oldLink = state.getDevice(state.selectedDevice);
+    let update = { ...oldLink, loss };
+
+    const arr = state.links.map((link) => {
+      if (link.id === oldLink.id) {
+        return update;
+      }
+      return link;
+    });
+
+    state.setLinks(arr);
+  }, [loss]);
+
+  /**
+   * handle bandwidth change
+   */
+  useEffect(() => {
+    let oldLink = state.getDevice(state.selectedDevice);
+    let update = { ...oldLink, bandwidth };
+
+    const arr = state.links.map((link) => {
+      if (link.id === oldLink.id) {
+        return update;
+      }
+      return link;
+    });
+
+    state.setLinks(arr);
+  }, [bandwidth]);
+
   return (
-    <div className="container">
-      <div className="field">
-        <CssTextField
-          id="linkDelay"
-          value={delay}
-          onChange={(event) => setDelay(event.target.value)}
-          label={"Delay (ms)"}
-        />
-      </div>
+    <ThemeProvider theme={theme}>
+      <div className="container">
+        <div className="field">
+          <CssTextField
+            id="linkDelay"
+            value={delay}
+            onChange={(event) => setDelay(event.target.value)}
+            label={"Delay (ms)"}
+          />
+        </div>
 
-      <div className="field">
-        <CssTextField
-          id="linkLoss"
-          value={loss}
-          onChange={(event) => setLoss(event.target.value)}
-          label={"Loss %"}
-        />
-      </div>
+        <div className="field">
+          <CssTextField
+            id="linkLoss"
+            value={loss}
+            onChange={(event) => setLoss(event.target.value)}
+            label={"Loss %"}
+          />
+        </div>
 
-      <div className="field">
-        <CssTextField
-          id="linkBandwith"
-          value={bandwith}
-          onChange={(event) => setBandwith(event.target.value)}
-          label={"Bandwith"}
-        />
-      </div>
+        <div className="field">
+          <CssTextField
+            id="linkBandwidth"
+            value={bandwidth}
+            onChange={(event) => setBandwidth(event.target.value)}
+            label={"Bandwidth"}
+          />
+        </div>
 
-      <div className="btn">
-        <Button
-          onClick={() => {
-            state.deleteDevice();
-          }}
-        >
-          Delete Link
-        </Button>
+        <div className="btn">
+          <Button
+            color="primary"
+            variant="contained"
+            size="small"
+            onClick={() => {
+              state.deleteDevice();
+            }}
+          >
+            Delete Link
+          </Button>
+        </div>
       </div>
-    </div>
+    </ThemeProvider>
   );
 };
 
